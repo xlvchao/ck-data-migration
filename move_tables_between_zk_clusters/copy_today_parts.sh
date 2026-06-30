a3783d414051d6db7f186b7f5b80fa9673cd7aac
xlvchao <33950671+xlvchao@users.noreply.github.com>
2026-06-30 09:57:26 +0800

    Create copy_today_parts.sh

---
+++ move_tables_between_zk_clusters/copy_today_parts.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# username="default"
+# password="Aiopschuat@2023"
+# database="aiops_local_test"
+
+username="default"
+password="Aiopschpro@2022"
+database="aiops_local_prd"
+
+target_table_suffix="20240506"
+
+echo "all start"
+
+rm -rf /data01/chwork/sql/
+mkdir -p /data01/chwork/sql/
+
+tables=("aiops_collect_100000093")
+for table in ${tables[@]}; do
+  echo "${table} start"
+
+  # source
+  sql="select data_paths from system.tables ARRAY JOIN data_paths where database='${database}' and name='${table}'"
+  path=$(clickhouse-client --host 127.0.0.1 --port 9000 --user $username --password $password --query "$sql")
+
+  # target
+  target_sql="select data_paths || 'detached' from system.tables ARRAY JOIN data_paths where database='${database}' and name = '${table}_${target_table_suffix}'"
+  target_path=$(clickhouse-client --host 127.0.0.1 --port 9000 --user $username --password $password --query "$target_sql")
+
+  # copy & attach
+  tdate=$(date -d "0 days ago" "+%Y%m%d")
+  echo "${table}-${tdate} start"
+
+  if [ "$(find "${path}" -maxdepth 1 -type d -name "${tdate}*" -o -type f -name "${tdate}*")" ]; then
+    cp -a ${path}${tdate}*  ${target_path}
+    attach_script=/data01/chwork/sql/${table}/${tdate}/attach_parts.sql
+    if [ ! -f ${attach_script} ]; then
+      mkdir -p /data01/chwork/sql/${table}/${tdate}/
+      touch ${attach_script}
+    fi
+    files=$(find "${target_path}" -name "${tdate}*")
+    for file in $files; do
+      filename=$(basename "$file")
+      attach_sql="ALTER TABLE ${database}.\`${table}_${target_table_suffix}\` ATTACH PART '${filename}';"
+      echo "$attach_sql" | awk '{print $0}' >> ${attach_script}
+    done
+    clickhouse --client --host 127.0.0.1 --port 9000 --user $username --password $password --ignore-error --multiquery < ${attach_script}
+  else
+    echo "${table}-${tdate} parts doesn't found!"
+  fi
+  echo "${table}-${tdate} finish"
+
+  echo "${table} finish"
+done
+
+echo "all finish"
